27545a84a1f962ed6b62dff0f3198d15aafdc791
E1win <87700553+E1win@users.noreply.github.com>
2022-03-20 15:15:15 +0100

    contactpage, accessibility, productimages
    created the contactform
    improved accessibility on the mobile navigationbuttons
    improved accessibility on informationcards
    enlarged the resolution on the mobile productimages

--- js/home.js
+++ js/home.js
@@ -6,11 +6,17 @@ const informationCards = document.querySelectorAll(".information__card");
 // FUNCTIONS
 const toggleInformationCard = function (evt) {
   let card = evt.currentTarget;
-
   card.classList.toggle("information__card-open");
+};
+
+const keyDownInformationCardHandler = function (evt) {
+  if (evt.code === "Enter") {
+    toggleInformationCard(evt);
+  }
 };
 
 // EVENT LISTENERS
 informationCards.forEach((element) => {
   element.addEventListener("click", toggleInformationCard);
+  element.addEventListener("keydown", keyDownInformationCardHandler);
 });
